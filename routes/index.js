ddcb2b9221efd61a24aaf960c1736b9e2462aec8
Jared Fuller <jaredshane@gmail.com>
2017-05-12 11:11:26 -0500

    required in the register function to handle when a user registers

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@
 
 const { Router } = require('express')
 const router = Router()
+const { register } = require('../controllers/userCtrl')
 
 router.get('/', (req, res) => {
   res.json({
-    
+    'hi': 'hi'
   })
 })
+
+router.post('/register', register)
 
 module.exports = router
