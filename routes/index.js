586e2022e3b61fdbd68471cf149849ee9c6ef864
Jared Fuller <jaredshane@gmail.com>
2017-05-12 12:12:42 -0500

    required in the login function

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 
 const { Router } = require('express')
 const router = Router()
-const { register } = require('../controllers/userCtrl')
+const { register, login } = require('../controllers/userCtrl')
 
 router.get('/', (req, res) => {
   res.json({
