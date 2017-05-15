4d067744d0b7ab4ec6dc9473a3221b236ce83850
Jared Fuller <jaredshane@gmail.com>
2017-05-15 17:16:30 -0500

    set the testing for getting an array of trips

--- test/users.spec.js
+++ test/users.spec.js
@@ -7,52 +7,72 @@ const server = require('../app')
 const {knex} = require('../db/database')
 chai.use(chaiHttp)
 
-describe('register user', () => {
-  // beforeEach(() => {
-  //   return knex.migrate.rollback()
-  //   .then(() => {
-  //     return knex.migrate.latest()
-  //   })
-  //   .then( () => {
-  //     return knex.seed.run()
-  //   })
-  // })
+// describe('register and login user', () => {
+//   beforeEach(() => {
+//     return knex.migrate.rollback()
+//     .then(() => {
+//       return knex.migrate.latest()
+//     })
+//     .then( () => {
+//       return knex.seed.run()
+//     })
+//   })
+//
+//   describe('POST /v1/register', () => {
+//     it('should post a new user to the database', () => {
+//       return chai.request(server)
+//       .post('/v1/register')
+//       .send({
+//         email: 'a@a.com',
+//         password: '111111'
+//       })
+//       .then( (res) => {
+//         // console.log(res.body)
+//         res.should.be.json
+//         res.body.user.should.be.a('array')
+//         res.body.user[0].should.have.property('id')
+//         res.body.user[0].should.have.property('email')
+//         res.body.user[0].should.have.property('password')
+//         res.body.user[0].email.should.equal('a@a.com')
+//       })
+//     })
+//   })
+//
+//   describe('POST /v1/login', () => {
+//     it('should login a user', () => {
+//       return chai.request(server)
+//       .post('/v1/login')
+//       .send({
+//         email: 'a@a.com',
+//         password: '111111'
+//       })
+//       .then((res) => {
+//         console.log(res)
+//       })
+//     })
+//   })
+// })
 
-  describe('POST /v1/register', () => {
-    it('should post a new user to the database', () => {
-      return chai.request(server)
-      .post('/v1/register')
-      .send({
-        email: 'a@a.com',
-        password: '111111'
-      })
-      .then( (res) => {
-        // console.log(res.body)
-        res.should.be.json
-        res.body.user.should.be.a('array')
-        res.body.user[0].should.have.property('id')
-        res.body.user[0].should.have.property('email')
-        res.body.user[0].should.have.property('password')
-        res.body.user[0].email.should.equal('a@a.com')
-      })
+describe('handle trip routes', () => {
+  beforeEach(() => {
+    return knex.migrate.rollback()
+    .then(() => {
+      return knex.migrate.latest()
+    })
+    .then( () => {
+      return knex.seed.run()
     })
   })
 
-  describe('POST /v1/login', () => {
-    it('should login a user', () => {
+  describe('GET /v1/trip/:id', () => {
+    it('should get get all the trips for a user\' id', () => {
       return chai.request(server)
-      .post('/v1/login')
-      .send({
-        email: 'a@a.com',
-        password: '111111'
-      })
+      .get('/v1/trip/2')
       .then((res) => {
-        console.log(res)
+        console.log(res.body)
+        res.body.should.be.a('array')
       })
     })
   })
-
-
-
 
 })
